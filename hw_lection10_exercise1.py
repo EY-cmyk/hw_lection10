7d102d25c821905583b49d8aeff4ae67939699be
EY-cmyk <61290430+EY-cmyk@users.noreply.github.com>
2020-03-23 13:54:50 +0200

    Add files via upload

---
+++ hw_lection10_exercise1.py
@@ -0,0 +1,17 @@
+import math
+import colorama
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+
+class Person():
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+    def talk(self):
+        print("Здравствуйте, меня зовут" + '' + self.name.title() + '' +  self.surname.title() +'' +  'и мне' + '' +'' self.age +''+ ' лет')
+
+my_person = Person('Antony', 'Johnson', '35')
+my_person.talk()
